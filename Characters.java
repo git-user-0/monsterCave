28f7e2a527835b7f12ff1b338d68ca7d928fe250
git-user-0 <64606499+git-user-0@users.noreply.github.com>
2020-05-08 12:33:19 +0100

    added a method to handle fights
    Added a fight method to the class Characters to avoid having to type it again and again in other classes.

--- Characters.java
+++ Characters.java
@@ -10,5 +10,19 @@ public class Characters {
     // define a method to return a random integer
     public static int random(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max);
+        
+    // define a method to have the player and monster fight
+    static void fight (int pAttack, int mAttack, String mName) {
+        pAttack += random(1, 7);
+        mAttack += random(1, 7);
+        if (pAttack >= mAttack) {
+            System.out.format("%n%nYour attack strength is: %d", pAttack);
+            System.out.format("%n%s attack strength is %d", mName, mAttack);
+            System.out.format("%n%nWell done - you have defeated the %s", mName);
+        } else {
+            System.out.format("%n%nYour attack strength is: %d", pAttack);
+            System.out.format("%n%s attack strength is %d", mName, mAttack);
+            System.out.format("%n%nSadly, you have been defeated - your quest ends here!");
+        }
     }
 }
